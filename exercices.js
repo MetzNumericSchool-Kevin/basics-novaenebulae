c8c07ba260abf3a1225caeea80223459b50c374d
Lucas Olivarez <74412774+novaenebulae@users.noreply.github.com>
2025-02-07 15:41:14 +0100

    Add exercice 2

--- exercices.js
+++ exercices.js
@@ -8,3 +8,11 @@ let healPotionCount = 50;
 let healPotionPrice = 5;
 let isShopOpen = true;
 
+// Exercice 2 : Affichage conditionnel
+
+if (isShopOpen) {
+    console.log("Bienvenue dans la boutique " + shopName + " Aventurier ! 🎉");
+} else {
+    console.log(
+      "La boutique "+ shopName + " est fermée, revenez plus tard Aventurier ! 😴");
+}
