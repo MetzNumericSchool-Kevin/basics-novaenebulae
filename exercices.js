5783e7bdd658ab392e9bc9745709fa20f50b2afd
Lucas Olivarez <74412774+novaenebulae@users.noreply.github.com>
2025-02-07 15:40:44 +0100

    Add exercice 1

---
+++ exercices.js
@@ -0,0 +1,10 @@
+const coin = "🪙";
+
+// Exercice 1 : Définition de variables
+
+const shopName = "Potions d'Archibald";
+
+let healPotionCount = 50;
+let healPotionPrice = 5;
+let isShopOpen = true;
+
